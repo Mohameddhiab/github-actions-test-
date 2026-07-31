309a302ae68bb197c6c34f0ca32cb6140577685f
Mohameddhiab <dhiabmohamed2003@gmail.com>
2026-07-31 20:47:21 +0100

    Add shout function to greet module

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 function greet(name:string){
     return `hello, ${name}!`;
 }
+
+function shout(name:string){
+    return `HELLO, ${name.toUpperCase()}!`;
+}
+
 module.exports=greet;
+module.exports.shout=shout;
 if(require.main===module){
     console.log(greet('world'));
 }
